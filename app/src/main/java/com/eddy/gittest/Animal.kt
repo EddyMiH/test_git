10f0b91190cafc6e5d3bf86ffe16d98688df8383
Eddy Hovhannisyan <eddy.hovhannisyan@gmail.com>
2021-09-26 16:29:19 +0400

    add new feature

--- app/src/main/java/com/eddy/gittest/Animal.kt
+++ app/src/main/java/com/eddy/gittest/Animal.kt
@@ -3,5 +3,5 @@ package com.eddy.gittest
 /**
  * Created by Eddy MiGoder on 9/26/2021
  */
-class Animal {
+sealed class Animal {
 }
--- app/src/main/java/com/eddy/gittest/MainActivity.kt
+++ app/src/main/java/com/eddy/gittest/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // super call
         super.onCreate(savedInstanceState)
+        emptyFunction()
         setContentView(R.layout.activity_main)
     }
+
+    private fun emptyFunction() = Unit
 }
